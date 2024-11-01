dfe6220ecf3568ae504000e3ed0759ede3f1042b
Antik Adhya <78850467+Antik004@users.noreply.github.com>
2024-11-01 12:18:56 +0530

    Time: 137 ms (5.63%), Space: 45.2 MB (57.75%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int l=nums.length;
+        int i,j;
+       
+        for(i=0;i<l;i++)
+        {
+            boolean found=false;
+            for(j=0;j<l;j++)
+            {
+                
+                if(i==nums[j])
+                {
+                    found=true;
+                    break;
+
+                }
+            }
+                if(!found)
+                {
+                    return i;
+                }
+        }
+        return l;
+    }
+}
