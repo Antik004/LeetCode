1f0f13765b3e55264a039382b7dd19bcfefc938d
Antik Adhya <78850467+Antik004@users.noreply.github.com>
2024-12-25 10:08:19 +0530

    Time: 19 ms (53.71%), Space: 55.4 MB (85.28%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,20 +1,23 @@
-import java.util.*;
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        for(int i=0;i<nums.length;i++)
-        {
-            int x=nums[i];
-            int j=0;
-            while(j <nums.length)
-            {
-                if(i!=j && nums[j]==x && Math.abs(i-j)<=k)
-                {
-                    return true;
-                }
-                j++;
-                
+        // A set to maintain a window of at most k elements
+        Set<Integer> window = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            // If the current number already exists in the window, we found a duplicate
+            if (window.contains(nums[i])) {
+                return true;
+            }
+
+            // Add the current number to the window
+            window.add(nums[i]);
+
+            // Maintain the size of the window to at most k
+            if (window.size() > k) {
+                window.remove(nums[i - k]); // Remove the oldest element from the window
             }
         }
+
         return false;
     }
 }
