57b0dbfd54823a8d1aea4ee12b762cde668da106
Antik Adhya <78850467+Antik004@users.noreply.github.com>
2025-08-23 19:59:09 +0530

    Runtime: 1 ms (61.97%), Memory: 42 MB (18.27%) - Gitcode

--- 14-longest-common-prefix/14. Longest Common Prefix.java
+++ 14-longest-common-prefix/14. Longest Common Prefix.java
@@ -7,7 +7,7 @@ class Solution {
         for(int i=0;i<strs[0].length();i++)
         {
             char c=strs[0].charAt(i);
-            for(int j=0;j<strs.length;j++)
+            for(int j=1;j<strs.length;j++)
             {
                 if(i>=strs[j].length() || strs[j].charAt(i)!=c)
                 {
