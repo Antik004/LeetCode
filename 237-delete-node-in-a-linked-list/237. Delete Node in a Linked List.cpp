f3548c7996512a0b3e0e03786368377895576794
Antik Adhya <78850467+Antik004@users.noreply.github.com>
2025-08-30 10:00:13 +0530

    Runtime: 7 ms (50.28%), Memory: 12.3 MB (83.13%) - Gitcode

--- 237-delete-node-in-a-linked-list/237. Delete Node in a Linked List.cpp
+++ 237-delete-node-in-a-linked-list/237. Delete Node in a Linked List.cpp
@@ -9,8 +9,8 @@
 class Solution {
 public:
     void deleteNode(ListNode* node) {
-        ListNode* temp=node->next;
-        node->val=temp->val;
-        node->next=temp->next;
+        ListNode* temp=node->next;   // the given *node is actually the link part of the prev node
+        node->val=temp->val;         //we copy the link part and get the actual node which contains the value to delete; next to that we take that link part and attach it to the next node to the deletable node    
+        node->next=temp->next;//here the node val is changed to the next node's val cuz we did node->val=temp->val where temp is actually the third node
     }
 };
