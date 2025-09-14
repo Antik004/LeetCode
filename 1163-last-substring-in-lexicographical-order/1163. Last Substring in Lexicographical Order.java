4a22a74fd86a5d1962c3c8c8d2d0b0813ea993fa
Antik Adhya <78850467+Antik004@users.noreply.github.com>
2025-09-14 16:20:22 +0530

    Runtime: 16 ms (62.98%), Memory: 46.3 MB (65.75%) - Gitcode

--- 1163-last-substring-in-lexicographical-order/1163. Last Substring in Lexicographical Order.java
+++ 1163-last-substring-in-lexicographical-order/1163. Last Substring in Lexicographical Order.java
@@ -1,16 +1,16 @@
 class Solution {
     public String lastSubstring(String s) {
-        int n=s.length();
 
+        int n=s.length();
         int i=0,k=0,j=1;
 
         while(j+k<n)
         {
-            if(s.charAt(i+k)==(s.charAt(j+k)))
+            if(s.charAt(i+k)==s.charAt(j+k))
             {
                 k++;
             }
-            else if(s.charAt(i+k)<s.charAt(j+k))
+            else if(s.charAt(i+k)<=s.charAt(j+k))
             {
                 i=Math.max(i+k+1,j);
                 j=i+1;
@@ -23,5 +23,5 @@ class Solution {
             }
         }
         return s.substring(i);
-    }    
+    }
 }
